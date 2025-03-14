0960df1d0e5f3bdfdb9b92c6449b827f7c99e8c8
Alessio Olivieri <alessio.olivieri@enginius.com>
2025-03-14 16:57:07 +0100

    1st commit

---
+++ src/main/java/it/olly/stripe/JavaSpringbootStripeBeApplication.java
@@ -0,0 +1,13 @@
+package it.olly.stripe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootStripeBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringbootStripeBeApplication.class, args);
+	}
+
+}
